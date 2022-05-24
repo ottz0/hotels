8b524526f8d8cb3ac670829cdc760df445db6cbc
Steve Watson <steve.watson@serversaustralia.com.au>
2022-05-24 12:44:04 +1000

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Hotel;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,27 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $hotelNames = DB::table('hotels')->insert([
+            ['name' => 'The Anchorage'],
+            ['name' => 'Ibis Budget Sydeny East'],
+            ['name' => 'Hilton Sydney'],
+            ['name' => 'Travelodge Hotel Sydney'],
+            ['name' => 'The Charrington Hotel of Chatswood'],
+            ['name' => 'YEHS Hotel Sydney CBD'],
+            ['name' => 'Great Souther Hotel Sydney'],
+            ['name' => 'Park Hyat Sydney'],
+        ]);
+
+
+        //Hotel::factory()->count(8)->create();
+
+
+        // Hotel::factory()->count(8)->make()->each(function($hotelNames) use ($hotels){
+        //     $comment->blog_post_id = $posts->random()->id;
+        //     $comment->save();
+        // });
+
+
+
     }
 }
