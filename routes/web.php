d8cbf8b7826792105c625968c11f6fb0989b8d4d
Steve Watson <steve.watson@serversaustralia.com.au>
2022-07-13 15:45:54 +1000

    Update

--- routes/web.php
+++ routes/web.php
@@ -27,34 +27,21 @@ Route::get('/form-login', function () {
 Route::get('/infopack', function () {
     return view('forms-singles.form-infopack');
 });
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::get('/buy/dedicated-servers/blaze-dedicated-servers', function () {
-//     return view('buy.dedicated-servers.blaze');
-// });
-
-Route::resource('marketplace/dedicated-servers', ServerController::class);
-
-//Route::get('/', [EpsRapidController::class, 'test'])->name('welcome');
-
 Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [HomeController::class, 'home'])->name('welcome');
 
 
 
-Route::resource('accommodation/{accommodation_type}/accommodations', HotelController::class);
+//Route::get('/', [EpsRapidController::class, 'test'])->name('welcome');
 
+Route::resource('accommodation/{accommodation_type}/accommodations', HotelController::class);
 Route::resource('hotels', HotelController::class);
 Route::resource('hotels.reviews', HotelReviewController::class);
 
 
 
+Route::resource('marketplace/dedicated-servers', ServerController::class);
 
 
-//nrl/team/players
-
-//marketplace/{product_category}/{products}/{id}
-
-//accommodation/{accommodation_type}/{accommodations}/{id}
